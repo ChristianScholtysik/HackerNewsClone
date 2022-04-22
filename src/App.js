2c92e88d98b37809e1874cb934a5a97f22ee134a
ChristianScholtysik <99259326+ChristianScholtysik@users.noreply.github.com>
2022-04-22 08:21:38 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import News from './components/News'
+
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+   
+ 
+      <News/> 
+     
+     <div>
+     
+     </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
